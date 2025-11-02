62e6137f7fe81e24ce752ebfa62e4d9a19ebe4b0
riteshkumar1601 <riteshkumarr1601@gmail.com>
2025-11-02 11:04:19 +0530

    live on google

--- src/components/contactUs/contactus.jsx
+++ src/components/contactUs/contactus.jsx
@@ -21,7 +21,7 @@ const ContactUs = ({ onBookMeetingClick }) => {
               <div className="w-full lg:w-1/2 flex justify-center order-1 lg:order-2">
                 <img
                   src="/contactus_img.png"
-                  alt="Contact Us"
+                  alt="Visvas Contact Us"
                   className="w-full  lg:max-w-lg h-auto rounded-lg shadow-lg"
                 />
               </div>
